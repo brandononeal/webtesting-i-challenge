5506d847a8fc2de91cd579b288b8baa59723a023
brandononeal <baoneal95@gmail.com>
2020-12-16 14:21:03 -0800

    mvp complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,23 @@ module.exports = {
 };
 
 function success(item) {
+  if (item.enhancement < 20) {
+    item.enhancement++;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability -= 5;
+  } else if (item.enhancement > 14) {
+    item.durability -= 10;
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
